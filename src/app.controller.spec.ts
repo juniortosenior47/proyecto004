a1e5ebafa46acdc325f0afd1ec462003934996e6
juanmohe <julian.grisalesg@gmail.com>
2024-06-03 21:24:18 -0500

    se adiciona prueba unitaria

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CompressDTO } from './request.dto';
+import { ResponseDTO } from './response.dto';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -16,7 +18,22 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      const request: CompressDTO = { text: 'Eraseunavez.' };
+      const response: ResponseDTO[] = [
+        { order: 0, row: 0, column: 0 },
+        { order: 1, row: 0, column: 1 },
+        { order: 2, row: 0, column: 2 },
+        { order: 3, row: 0, column: 3 },
+        { order: 4, row: 1, column: 0 },
+        { order: 5, row: 1, column: 1 },
+        { order: 6, row: 1, column: 2 },
+        { order: 2, row: 1, column: 3 },
+        { order: 8, row: 2, column: 0 },
+        { order: 4, row: 2, column: 1 },
+        { order: 10, row: 2, column: 2 },
+        { order: 11, row: 2, column: 3 },
+      ];
+      expect(appController.compress(request)).toBe(response);
     });
   });
 });
